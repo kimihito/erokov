32390edbade3d84f8be87367654ff1f6c229ca62
kimihito <tatsurotamashiro@gmail.com>
2012-08-27 19:33:11 +0900

    add markov method

--- erokov.py
+++ erokov.py
@@ -1,40 +1,44 @@
 #!/usr/bin/env python
 # coding: utf-8
+
+#AVのタイトルをマルコフ連鎖で作るスクリプトを書いてます
+
 import random
 import MeCab
 
 def wakati(text):
     t = MeCab.Tagger("-Owakati")
     m = t.parse(text)
-    
-    return m
+    result = m.split(" ")
+    return result
 
 if __name__ == "__main__":
-    filename = "title_sort_uniq2.txt"
+    filename = "title_sort_uniq.txt"
     wordlist = []
     src = open(filename,"r").read().split("\n")
     for tmpsrc in src:
-        print tmpsrc
         wordlist += wakati(tmpsrc)
-        print wordlist
-#    erokov = {}
-#    w1 = ""
-#    w2 = ""
-#    for word in wordlist:
-#        if w1 and w2:
-#            if(w1,w2) not in erokov:
-#                erokov[(w1,w2)] = []
-#            erokov[(w1,w2)].append(word)
-#        w1,w2 = w2, word
-#       
-#    count = 0
-#    sentence = ""
-#    w1,w2 = random.choice(erokov.keys())
-#    while count < len(wordlist):
-#        tmp = random.choice(erokov[(w1,w2)])
-#        sentence += tmp
-#        w1,w2 = w2, tmp
-#        count += 1
-#
-#
-#    print sentence
+    erokov = {}
+    w1 = ""
+    w2 = ""
+    for word in wordlist:
+        if w1 and w2:
+            if (w1,w2) not in erokov:
+                erokov[(w1,w2)] = []
+            erokov[(w1,w2)].append(word)
+        w1,w2 = w2, word
+       
+    count = 0
+    sentence = ""
+    w1,w2 = random.choice(erokov.keys())
+    while count < 11:
+        try:
+            tmp = random.choice(erokov[(w1,w2)])
+            sentence += tmp
+            w1,w2 = w2, tmp
+            count += 1
+        except KeyError:
+            print "Error!"
+            pass
+
+    print sentence 
---
+++ tweet.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+# coding: utf-8
+import tweepy
+import erokov
+
+auth = tweepy.OAuthHandler("F5SGdjdqHgnHjWsUV6vOw",
+"nyfM3eIl07v9eetZSY7rU9QlIXkKzQpzNvMP9H8iU") 
+
+auth.get_oauthrization_url
+
